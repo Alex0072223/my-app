f799a3ed374ed5a1f5c67f69a24c70843b3c9242
alex <alex0072223@mail.ru>
2022-05-15 23:57:27 +0400

    Y

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './ErrorPageStyle.css'
+import {Link} from "react-router-dom";
 
 const ErrorPage = () => {
     return (
         <div>
+            <ul>
+                <li><Link to={'/transfer'}><h5>На главную (Transfer page)</h5></Link></li>
+                <li><Link to={'/sort'}><h5>Сортировка по id</h5></Link></li>
+                <li><Link to={'/'}><h5>Новости</h5></Link></li>
+            </ul>
             <h1 className="svast">404</h1>
         </div>
     );
