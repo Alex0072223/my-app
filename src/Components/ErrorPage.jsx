b69f92c27eac72fe282aa71505ced8ae32ae4d76
alex <alex0072223@mail.ru>
2022-05-18 07:28:00 +0400

    Y

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -6,8 +6,10 @@ const ErrorPage = () => {
     return (
         <div>
             <ul>
-                <li><Link to={'/transfer'}><h5>На главную (Transfer page)</h5></Link></li>
-                <li><Link to={'/sort'}><h5>Сортировка по id</h5></Link></li>
+               {/* <li><Link to={'/transfer'}><h5>На главную (Transfer page)</h5></Link></li>
+                <li><Link to={'/sort'}><h5>Сортировка по id</h5></Link></li>*/}
+                <li><Link to={'/about'}><h5>Про автора</h5></Link></li>
+                <li><Link to={'/bookmarks'}><h5>Закладки</h5></Link></li>
                 <li><Link to={'/'}><h5>Новости</h5></Link></li>
             </ul>
             <h1 className="svast">404</h1>
